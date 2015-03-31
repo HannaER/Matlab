45f6ee040906e9614fedd86e0360728f68265c13
HannaER <hannaruner@gmail.com>
2015-03-31 10:38:53 +0200

    tofilehammingh
    organiserat upp mappen dessutom

--- SIT2/speech-recognition/Tester/tests.m
+++ SIT2/speech-recognition/Tester/tests.m
@@ -65,16 +65,18 @@ clc;
 N = 160;
 ham = hamming(N);
 
-
-temp = '';
+%extern float const pm hamming
+temp = 'extern float const pm hamming[BLOCK_LENGTH] = {';
 for i = 1:length(ham)
     %if i ~= length(x)
-    temp = strcat(temp, 'h[',num2str( i -1) , ']= ', num2str(ham(i)), '; \n' );
+    temp = strcat(temp,'\n\t\t', num2str(ham(i)), ', ' );
     %else
     %temp = strcat(temp, num2str(x(i)));
     %end
 end
-
+temp = strcat(temp, '\n}; \n\n', '');
+file = fopen('hammingh.txt', 'w');
+fprintf(file, temp);
 
 
 %%
