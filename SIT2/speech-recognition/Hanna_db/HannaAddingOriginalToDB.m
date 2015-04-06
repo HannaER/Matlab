2ef183ca3779e8bd22ed4f7999885cbb4a30283e
HannaER <hannaruner@gmail.com>
2015-04-06 19:31:23 +0200

    new db

--- SIT2/speech-recognition/Hanna_db/HannaAddingOriginalToDB.m
+++ SIT2/speech-recognition/Hanna_db/HannaAddingOriginalToDB.m
@@ -36,22 +36,22 @@ load Hanna_db/original/v9.mat
 add_to_db(rec, 'Hanna_db/database/l9', L, D, P, M, 8000, GAMMA, 0);
 
 load Hanna_db/original/v10.mat
-add_to_db(rec, 'Hanna_db/database/l10', L, D, P, M, 8000, GAMMA, 0);
+add_to_db(rec(1000:end), 'Hanna_db/database/l10', L, D, P, M, 8000, GAMMA, 0);
 
 load Hanna_db/original/v11.mat
-add_to_db(rec, 'Hanna_db/database/l11', L, D, P, M, 8000, GAMMA, 0);
+add_to_db(rec(1000:end), 'Hanna_db/database/l11', L, D, P, M, 8000, GAMMA, 0);
 
 load Hanna_db/original/v12.mat
-add_to_db(rec, 'Hanna_db/database/l12', L, D, P, M, 8000, GAMMA, 0);
+add_to_db(rec(1000:end), 'Hanna_db/database/l12', L, D, P, M, 8000, GAMMA, 0);
 
 load Hanna_db/original/v13.mat
-add_to_db(rec, 'Hanna_db/database/l13', L, D, P, M, 8000, GAMMA, 0);
+add_to_db(rec(1000:end), 'Hanna_db/database/l13', L, D, P, M, 8000, GAMMA, 0);
 
 load Hanna_db/original/v14.mat
-add_to_db(rec, 'Hanna_db/database/l14', L, D, P, M, 8000, GAMMA, 0);
+add_to_db(rec(1000:end), 'Hanna_db/database/l14', L, D, P, M, 8000, GAMMA, 0);
 
 load Hanna_db/original/v15.mat
-add_to_db(rec, 'Hanna_db/database/l15', L, D, P, M, 8000, GAMMA, 0);
+add_to_db(rec(1000:end), 'Hanna_db/database/l15', L, D, P, M, 8000, GAMMA, 0);
 
 
 
@@ -84,22 +84,22 @@ load Hanna_db/original/h9.mat
 add_to_db(rec, 'Hanna_db/database/r9', L, D, P, M, 8000, GAMMA, 0);
 
 load Hanna_db/original/h10.mat
-add_to_db(rec, 'Hanna_db/database/r10', L, D, P, M, 8000, GAMMA, 0);
+add_to_db(rec(1000:end), 'Hanna_db/database/r10', L, D, P, M, 8000, GAMMA, 0);
 
 load Hanna_db/original/h11.mat
-add_to_db(rec, 'Hanna_db/database/r11', L, D, P, M, 8000, GAMMA, 0);
+add_to_db(rec(1000:end), 'Hanna_db/database/r11', L, D, P, M, 8000, GAMMA, 0);
 
 load Hanna_db/original/h12.mat
-add_to_db(rec, 'Hanna_db/database/r12', L, D, P, M, 8000, GAMMA, 0);
+add_to_db(rec(1000:end), 'Hanna_db/database/r12', L, D, P, M, 8000, GAMMA, 0);
 
 load Hanna_db/original/h13.mat
-add_to_db(rec, 'Hanna_db/database/r13', L, D, P, M, 8000, GAMMA, 0);
+add_to_db(rec(1000:end), 'Hanna_db/database/r13', L, D, P, M, 8000, GAMMA, 0);
 
 load Hanna_db/original/h14.mat
-add_to_db(rec, 'Hanna_db/database/r14', L, D, P, M, 8000, GAMMA, 0);
+add_to_db(rec(1000:end), 'Hanna_db/database/r14', L, D, P, M, 8000, GAMMA, 0);
 
 load Hanna_db/original/h15.mat
-add_to_db(rec, 'Hanna_db/database/r15', L, D, P, M, 8000, GAMMA, 0);
+add_to_db(rec(1000:end), 'Hanna_db/database/r15', L, D, P, M, 8000, GAMMA, 0);
 
 
 
